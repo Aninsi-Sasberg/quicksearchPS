bedd74d7f0417a6aa505a8ee5086f1bc9ba8250e
Aninsi Sasberg <Maximilian.Bernhard@vodafone.de>
2022-08-03 12:17:28 +0200

    feat: add quicksearchPS.ps1

---
+++ quicksearchPS.ps1
@@ -0,0 +1,20 @@
+function Get-NewSearch {
+    $global:continue = Read-Host -Prompt "`nStart new Search? ('Y' or 'y' to start new search)"
+    if ($global:continue -ne "y" -and $global:continue -ne "Y") {
+        break
+    }
+}
+
+while ($TRUE){
+    
+    $getsearchfor = Read-Host -Prompt "What do you want to search for?`n"
+    $searchin = Read-Host -Prompt "`nWhich directory do you want to search in?`n"
+    $searchfor = "*"+$getsearchfor+"*"
+    Write-Output "`n"
+
+    
+    Get-ChildItem -Path $searchin -Filter $searchfor -Recurse -Force | ForEach-Object { $_.FullName }
+    Write-Output "`n[Finished]"
+    Get-NewSearch
+
+}
